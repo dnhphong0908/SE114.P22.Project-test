a3d2a90c91f48e9ba30642e6f63c1370d52baf1a
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-03-16 13:28:37 +0700

    Fix regex patterns in TypeUtil for phone and email validation

--- backend/src/main/java/com/se114p12/backend/util/TypeUtil.java
+++ backend/src/main/java/com/se114p12/backend/util/TypeUtil.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TypeUtil {
-    public static final String PHONE_REGEX = "^(03|05|07|08|09)[0-9]{8}$\n";
-    public static  final String EMAIL_REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$\n";
+    public static final String PHONE_REGEX = "^(03|05|07|08|09)[0-9]{8}$";
+    public static  final String EMAIL_REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
 
     public static int checkUsernameType(String username) {
         if (username.matches(PHONE_REGEX)) {
