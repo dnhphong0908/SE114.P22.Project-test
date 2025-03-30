b1a0a860fd5450b1cd7c5c2f7cfe1c61b8409d25
KhangPham205 <ptkhang17122005@gmail.com>
2025-03-30 18:09:48 +0700

    Add Get to ProductController

--- backend/src/main/java/com/se114p12/backend/controller/ProductController.java
+++ backend/src/main/java/com/se114p12/backend/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.se114p12.backend.controller;
 import com.se114p12.backend.domain.Product;
 import com.se114p12.backend.service.ProductService;
 
+import com.se114p12.backend.vo.PageVO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
     public ProductController(ProductService productService) { this.productService = productService; }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<PageVO<Product>> getProductsByCategory(
+            @PathVariable("id") Long categoryId, Pageable pageable) {
+        return ResponseEntity.ok(productService.getProductsByCategory(categoryId, pageable));
+    }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
