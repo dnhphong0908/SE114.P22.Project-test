cd29b913d9740802c6df6ea98ded96ab122fb6be
CaHoiAu <trantranchau1605@gmail.com>
2025-04-11 15:40:53 +0700

    Thêm ChangePasswordScreen

--- frontend/MAM/app/src/main/java/com/example/mam/Screen/component/TextField.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/Screen/component/TextField.kt
@@ -1,19 +1,14 @@
 package com.example.mam.Screen.component
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -26,9 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -38,11 +30,8 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.mam.R
 import com.example.mam.ui.theme.BrownDefault
-import com.example.mam.ui.theme.OrangeDefault
-import com.example.mam.ui.theme.OrangeLight
-import com.example.mam.ui.theme.OrangeLighter
+import com.example.mam.ui.theme.GreyLight
 import com.example.mam.ui.theme.Transparent
 import com.example.mam.ui.theme.Variables
 import com.example.mam.ui.theme.WhiteDefault
