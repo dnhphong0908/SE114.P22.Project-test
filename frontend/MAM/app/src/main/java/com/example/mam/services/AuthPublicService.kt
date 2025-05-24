0f0433522c82231e20e29934b75e8c2affe635f0
CaHoiAu <trantranchau1605@gmail.com>
2025-05-24 23:16:19 +0700

    Thêm ở các file AuthPublicService

--- frontend/MAM/app/src/main/java/com/example/mam/services/AuthPublicService.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/services/AuthPublicService.kt
@@ -1,7 +1,9 @@
 package com.example.mam.services
 
+import com.example.mam.dto.authentication.SendOTPRequest
 import com.example.mam.dto.authentication.SignInRequest
 import com.example.mam.dto.authentication.SignInResponse
+import com.example.mam.dto.authentication.VerifyOTPRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -11,4 +13,8 @@ interface AuthPublicService {
     suspend fun login(@Body request: SignInRequest): SignInResponse
     @POST("auth/register")
     suspend fun signUp(@Body request: SignInRequest)
+    @POST("auth/send-otp")
+    suspend fun sendOtp(@Body request: SendOTPRequest)
+    @POST("auth/verify-otp")
+    suspend fun verifyOtp(@Body request: VerifyOTPRequest)
 }
--- frontend/MAM/app/src/main/java/com/example/mam/services/UserService.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/services/UserService.kt
@@ -1,5 +1,13 @@
 package com.example.mam.services
 
-interface UserService {
+import com.example.mam.dto.user.UserResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
+interface UserService {
+    @GET("users/{userId}")
+    suspend fun getUserById(@Path("id") userId: String): Response<UserResponse>
 }
