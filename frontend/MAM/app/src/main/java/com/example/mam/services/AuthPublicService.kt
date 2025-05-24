0ac0129c722e41eb7e14bd3150580082e3e5fe07
Gnuttttt <dinhthanhtung0312@gmail.com>
2025-05-24 14:04:12 +0700

    userService example

--- frontend/MAM/app/src/main/java/com/example/mam/services/AuthPublicService.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/services/AuthPublicService.kt
@@ -1,19 +1,11 @@
 package com.example.mam.services
 
-import com.example.mam.entity.Notification
-import com.example.mam.entity.Order
-import com.example.mam.entity.User
 import com.example.mam.dto.signin.SignInRequest
 import com.example.mam.dto.signin.SignInResponse
-import okhttp3.MultipartBody
 import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Part
 
-interface Authpublicservice {
+interface AuthPublicService {
     @POST("auth/login")
     suspend fun login(@Body request: SignInRequest): SignInResponse
     @POST("auth/register")
--- frontend/MAM/app/src/main/java/com/example/mam/services/RetrofitClient.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/services/RetrofitClient.kt
@@ -1,16 +1,39 @@
 package com.example.mam.services
 
+import com.example.mam.entity.User
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://159.223.62.237/api/v1/"
+    private val authInterceptor = Interceptor { chain ->
+        val accessToken = "yourAccessToken" // Get dynamically if needed
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $accessToken")
+            .build()
+        chain.proceed(request)
+    }
 
-    val api: APIservice by lazy {
+    val okHttpClientWithAuth = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .build()
+
+    val authPublic: AuthPublicService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(APIservice::class.java)
+            .create(AuthPublicService::class.java)
+    }
+
+    val user: UserService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClientWithAuth)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserService::class.java)
     }
 }
