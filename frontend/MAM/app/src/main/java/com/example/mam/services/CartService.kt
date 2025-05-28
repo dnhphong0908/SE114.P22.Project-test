885bb09b84f708b7a315e26c4363e173b5e44904
CaHoiAu <trantranchau1605@gmail.com>
2025-05-28 23:26:21 +0700

    Thêm vài file ở Service

---
+++ frontend/MAM/app/src/main/java/com/example/mam/services/CartService.kt
@@ -0,0 +1,9 @@
+package com.example.mam.services
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface CartService {
+//    @POST("carts")
+//    suspend fun createNewCart(@Body cartRequest: CartRequest): Response<CartResponse>)
+}
