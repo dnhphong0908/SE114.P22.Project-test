f2ec76cfaadd30e25a37b885005581258e58bc96
CaHoiAu <trantranchau1605@gmail.com>
2025-05-29 12:20:17 +0700

    Thêm dto CartItem

--- frontend/MAM/app/src/main/java/com/example/mam/services/CartService.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/services/CartService.kt
@@ -4,6 +4,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface CartService {
-//    @POST("carts")
-//    suspend fun createNewCart(@Body cartRequest: CartRequest): Response<CartResponse>)
+    @POST("carts")
+    suspend fun createNewCart(@Body cartRequest: CartRequest): CartResponse
+
 }
