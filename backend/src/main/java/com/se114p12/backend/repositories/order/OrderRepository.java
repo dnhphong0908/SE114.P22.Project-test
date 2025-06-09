206fe9efea042cf0513a9e7d27775034fb9cc618
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-06-09 18:05:25 +0700

    🐛 fix(server): fix getOrderByUserId

--- backend/src/main/java/com/se114p12/backend/repositories/order/OrderRepository.java
+++ backend/src/main/java/com/se114p12/backend/repositories/order/OrderRepository.java
@@ -3,12 +3,6 @@ package com.se114p12.backend.repositories.order;
 import com.se114p12.backend.entities.order.Order;
 import java.time.Instant;
 import java.util.List;
-
-import org.springframework.data.domain.Limit;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -18,8 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository
     extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
-
-  Page<Order> findByUser_Id(Long userId, Specification<Order> spec, Pageable pageable);
 
   @Query(
       "SELECT o FROM Order o WHERE (:start IS NULL OR o.createdAt >= :start) AND (:end IS NULL OR"
